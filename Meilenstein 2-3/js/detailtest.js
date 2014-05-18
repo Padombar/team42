f71d97713f9c25ed615d6ead9556cdbef937b96d
SebastianAurin <sebastianaurin@hotmail.com>
2014-05-19 00:28:31 +0200

    CHANGED: tabmenu.css, detailtest.html, detailtest.js, Tabellen werden nun per javascript erzeugt mit hilfe von json objects; -s.aurin

--- Meilenstein 2-3/js/detailtest.js
+++ Meilenstein 2-3/js/detailtest.js
@@ -1,50 +1,140 @@
-         
-function messeTabelle(){
-    
-    var messe = new Array();
-        messe["CeBIT"] = "15 €";
-        messe["ConhIT"] = "30 €";
-        messe["WebTech Con"] = "300 €";
-            
-    for (var e in messe) {
-            document.write(
-            "<tr><td>" + e + "</td>" +
-            "<td><code>" + messe[e] + "</code></td></tr>"
-            );
+var webtechJSON = {"webtech":[
+    {
+        "name": "Spies",
+        "Vorname": "Marcell",
+        "Studiengang": "IB",
+        "eMail": "m.spies@gmail.com"
+    },
+    {
+        "name": "Hoppe",
+        "Vorname": "Johannes",
+        "Studiengang": "UIB",
+        "eMail": "j.hoppe@gmx.de"
+    },
+    {
+        "name": "Hofmeister",
+        "Vorname": "Johannes",
+        "Studiengang": "UIB",
+        "eMail": "cessor@gmail.de"
     }
-}
+]};
 
 
-function cebitTabelle(){   
-    
-    for (var e in cebit){
-        if (cebit.hasOwnProperty(e)) {
-            "<tr><td>" + print(e["name"]); + "</td>" +
-            "<tr><td>" + print(e["Vorname"]); + "</td>"
-        }
+
+
+  var conhitJSON ={"conhit": [
+    {
+        "name": "Schmuecker",
+        "Vorname": "Paul",
+        "Studiengang": "IMB",
+        "eMail": "p.schmuecker@hs-mannheim.de"
+    },
+    {
+        "name": "Felsenheimer",
+        "Vorname": "Jan",
+        "Studiengang": "IB",
+        "eMail": "j.felsenheimer@hs-mannheim.de"
     }
+]};
+
+     var cebitJSON ={"cebit":[
+      {
+          "name": "Ent",
+          "Vorname": "Thilo",
+          "Studiengang": "IB",
+          "eMail": "t.ent@gmail.de"
+      },
+      {
+          "name": "Kraus",
+          "Vorname": "Martina",
+          "Studiengang": "IB",
+          "eMail": "m.kraus@hs-mannheim.de"
+      },
+      {
+          "name": "Smits",
+          "Vorname": "Thomas",
+          "Studiengang": "UIB",
+          "eMail": "t.smits@hs-mannheim.de"
+      },
+      {
+          "name": "Groeschel",
+          "Vorname": "Michael",
+          "Studiengang": "UIB",
+          "eMail": "m.groeschel@hs-mannheim.de"
+      },
+      {
+          "name": "Ent",
+          "Vorname": "Thilo",
+          "Studiengang": "IB",
+          "eMail": "t.ent@gmail.de"
+      }
+  ]};
+
+      var kostenJSON = {"kosten":[
+      {
+          "Messe": "Cebit",
+          "Selbstkostenanteil": "15"
+      },
+
+      {
+          "Messe": "Conhit",
+          "Selbstkostenanteil": "50"
+      },
+
+      {
+          "Messe": "WebTechCon",
+          "Selbstkostenanteil": "300"
+      }
+  ]};
+
+
+function kostenTabelle(){
+
+    document.write('<table border="1" cellpadding="5" cellspacing="0">');
+    document.write(' <tr><th>Messe</th><th>Selbstkostenanteil</th></tr>');
+
+    for(i=0; i<kostenJSON.kosten.length;i++){
+        document.write(' <tr><td>'+kostenJSON.kosten[i].Messe+'</td><td>'+kostenJSON.kosten[i].Selbstkostenanteil+' €'+'</td></tr>');
+    }
+    document.write("</table>");
 }
 
+function cebitTabelle(){
 
+    document.write('<table border="1" cellpadding="5" cellspacing="0">');
+    document.write(' <tr><th>Name</th><th>Vorname</th><th>Studiengang</th><th>eMail</th></tr>');
 
-function conhitTabelle(){
-        for (var e in conhit){
-            document.write(
-            "<tr><td>" + e + "</td>" +
-            "<td><code>" + conhit[e] + "</code></td></tr>"
-            );
-        }
+    for(i=0; i<cebitJSON.cebit.length;i++){
+    document.write(' <tr><td>'+cebitJSON.cebit[i].name+'</td><td>'+cebitJSON.cebit[i].Vorname+'</td><td>'+cebitJSON.cebit[i].studiengang+'</td><td>'+cebitJSON.cebit[i].eMail+'</td></tr>');
+    }
+    document.write("</table>");
 }
-
 
 function webtechconTabelle(){
-        for (var e in webtechcon){
-            document.write(
-            "<tr><td>" + e + "</td>" +
-            "<td><code>" + webtechcon[e] + "</code></td></tr>"
-            );
-        }
+
+    document.write('<table border="1" cellpadding="5" cellspacing="0">');
+    document.write(' <tr><th>Name</th><th>Vorname</th><th>Studiengang</th><th>eMail</th></tr>');
+
+    for(i=0; i<webtechJSON.webtech.length;i++){
+        document.write(' <tr><td>'+webtechJSON.webtech[i].name+'</td><td>'+webtechJSON.webtech[i].Vorname+'</td><td>'+webtechJSON.webtech[i].studiengang+'</td><td>'+webtechJSON.webtech[i].eMail+'</td></tr>');
+    }
+    document.write("</table>");
 }
+
+function conhitTabelle(){
+
+    document.write('<table border="1" cellpadding="5" cellspacing="0">');
+    document.write(' <tr><th>Name</th><th>Vorname</th><th>Studiengang</th><th>eMail</th></tr>');
+
+    for(i=0; i<conhitJSON.conhit.length;i++){
+        document.write(' <tr><td>'+conhitJSON.conhit[i].name+'</td><td>'+conhitJSON.conhit[i].Vorname+'</td><td>'+conhitJSON.conhit[i].studiengang+'</td><td>'+conhitJSON.conhit[i].eMail+'</td></tr>');
+    }
+    document.write("</table>");
+}
+
+
+
+
 
 
 
