6cf380bc5357d3355e7c6df325ea01b8a5795f72
Padombar <pdthiele@gmail.com>
2014-05-18 13:16:12 +0200

    changed: tabmenu.js, tabellen visbility - p.dombrowski | added: detailtest.html-.js zum testen erstellt - p.dombrowski

--- Meilenstein 2-3/js/tabmenu.js
+++ Meilenstein 2-3/js/tabmenu.js
@@ -4,29 +4,44 @@ function teilnehmerContent(){
     document.getElementById("messe").style.backgroundColor = "transparent";
     document.getElementById("kostenTabelle").style.visibility = 'hidden';
     document.getElementById("teilnehmerTabBox").style.visibility = 'visible';
+    document.getElementById("cebitTabelle").style.visibility ='hidden';
+    document.getElementById("conitTabelle").style.visibility ='hidden';
+    document.getElementById("webtechconTabelle").style.visibility ='hidden';
 }
 
 function kostenContent(){
     document.getElementById("teilnehmer").style.backgroundColor ="transparent";
     document.getElementById("messe").style.backgroundColor = "#3f47cc";
-    document.getElementById("teilnehmerTabBox").style.visibility = 'hidden';
     document.getElementById("kostenTabelle").style.visibility ='visible';
+    document.getElementById("teilnehmerTabBox").style.visibility = 'hidden';
+    document.getElementById("cebitTabelle").style.visibility ='hidden';
+    document.getElementById("conitTabelle").style.visibility ='hidden';
+    document.getElementById("webtechconTabelle").style.visibility ='hidden';
 }
 
 function cebitContent(){
     document.getElementById("teilnehmerTabCebit").style.backgroundColor = "#00a4eb";
     document.getElementById("teilnehmerTabConhit").style.backgroundColor = "transparent";
     document.getElementById("teilnehmerTabWebConTech").style.backgroundColor = "transparent";
+    document.getElementById("cebitTabelle").style.visibility ='visible';
+    document.getElementById("conitTabelle").style.visibility ='hidden';
+    document.getElementById("webtechconTabelle").style.visibility ='hidden';
 }
 
 function conitContent(){
     document.getElementById("teilnehmerTabCebit").style.backgroundColor = "transparent";
     document.getElementById("teilnehmerTabConhit").style.backgroundColor = "#00a4eb";
     document.getElementById("teilnehmerTabWebConTech").style.backgroundColor = "transparent";
+    document.getElementById("cebitTabelle").style.visibility ='hidden';
+    document.getElementById("conitTabelle").style.visibility ='visible';
+    document.getElementById("webtechconTabelle").style.visibility ='hidden';
 }
 
 function webtechconContent(){
     document.getElementById("teilnehmerTabCebit").style.backgroundColor = "transparent";
     document.getElementById("teilnehmerTabConhit").style.backgroundColor = "transparent";
     document.getElementById("teilnehmerTabWebConTech").style.backgroundColor = "#00a4eb";
+    document.getElementById("cebitTabelle").style.visibility ='hidden';
+    document.getElementById("conitTabelle").style.visibility ='hidden';
+    document.getElementById("webtechconTabelle").style.visibility ='visible';
 }
