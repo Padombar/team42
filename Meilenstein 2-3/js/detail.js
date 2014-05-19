c82d9dd6e2ebe410b2ed7c8ba3ad40c9366c52d8
Maren <mary_dietrich@gmx.de>
2014-05-19 14:32:13 +0200

    Changed: detail.css, detail.js, detail.html, Zurück-Buttons, Benennungen, Kommentare, JS zusammen, Aufgabe3 - mdietrich

--- Meilenstein 2-3/js/detail.js
+++ Meilenstein 2-3/js/detail.js
@@ -1,3 +1,64 @@
+/* Tab Menuebereich */
+function teilnehmerContent(){
+    document.getElementById("teilnehmer").style.color ="#F9F9F9";
+    document.getElementById("messe").style.color ="#000000";
+    document.getElementById("teilnehmer").style.backgroundColor ="#3f47cc";
+    document.getElementById("messe").style.backgroundColor = "transparent";
+    document.getElementById("kostenTabelle").style.visibility = 'hidden';
+    document.getElementById("teilnehmerTabBox").style.visibility = 'visible';
+    document.getElementById("teilnehmerTabCebit").style.backgroundColor = "#00a4eb";
+    document.getElementById("teilnehmerTabConhit").style.backgroundColor = "transparent";
+    document.getElementById("teilnehmerTabWebConTech").style.backgroundColor = "transparent";
+    document.getElementById("teilnehmerTabellen").style.visibility ='visible';
+    document.getElementById("cebitTabelle").style.visibility ='visible';
+    document.getElementById("conhitTabelle").style.visibility ='hidden';
+    document.getElementById("webtechconTabelle").style.visibility ='hidden';
+}
+
+function kostenContent(){
+    document.getElementById("teilnehmer").style.color ="#000000";
+    document.getElementById("messe").style.color ="#F9F9F9";
+    document.getElementById("teilnehmer").style.backgroundColor ="transparent";
+    document.getElementById("messe").style.backgroundColor = "#3f47cc";
+    document.getElementById("kostenTabelle").style.visibility ='visible';
+    document.getElementById("teilnehmerTabBox").style.visibility = 'hidden';
+    document.getElementById("teilnehmerTabellen").style.visibility ='hidden';
+    document.getElementById("cebitTabelle").style.visibility ='hidden';
+    document.getElementById("conhitTabelle").style.visibility ='hidden';
+    document.getElementById("webtechconTabelle").style.visibility ='hidden';
+}
+
+function cebitContent(){
+    document.getElementById("teilnehmerTabCebit").style.backgroundColor = "#00a4eb";
+    document.getElementById("teilnehmerTabConhit").style.backgroundColor = "transparent";
+    document.getElementById("teilnehmerTabWebConTech").style.backgroundColor = "transparent";
+    document.getElementById("teilnehmerTabellen").style.visibility ='visible';
+    document.getElementById("cebitTabelle").style.visibility ='visible';
+    document.getElementById("conhitTabelle").style.visibility ='hidden';
+    document.getElementById("webtechconTabelle").style.visibility ='hidden';
+}
+
+function conhitContent(){
+    document.getElementById("teilnehmerTabCebit").style.backgroundColor = "transparent";
+    document.getElementById("teilnehmerTabConhit").style.backgroundColor = "#00a4eb";
+    document.getElementById("teilnehmerTabWebConTech").style.backgroundColor = "transparent";
+    document.getElementById("teilnehmerTabellen").style.visibility ='visible';
+    document.getElementById("cebitTabelle").style.visibility ='hidden';
+    document.getElementById("conhitTabelle").style.visibility ='visible';
+    document.getElementById("webtechconTabelle").style.visibility ='hidden';
+}
+
+function webtechconContent(){
+    document.getElementById("teilnehmerTabCebit").style.backgroundColor = "transparent";
+    document.getElementById("teilnehmerTabConhit").style.backgroundColor = "transparent";
+    document.getElementById("teilnehmerTabWebConTech").style.backgroundColor = "#00a4eb";
+    document.getElementById("teilnehmerTabellen").style.visibility ='visible';
+    document.getElementById("cebitTabelle").style.visibility ='hidden';
+    document.getElementById("conhitTabelle").style.visibility ='hidden';
+    document.getElementById("webtechconTabelle").style.visibility ='visible';
+}
+
+/* JSON-Objekte */
 var webtechJSON = {"webtech":[
     {
         "name": "Spies",
@@ -84,7 +145,7 @@ var webtechJSON = {"webtech":[
       }
   ]};
 
-
+/* Tabellen erstellen und mit JSON-Objekten beschreiben */
 function kostenTabelle() {
 
     document.write('<table class ="kostentable"  border="1" cellpadding="5" cellspacing="0">');
