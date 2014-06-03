fde62ff8ff98cd2e310d2c93dc51f0ef26badf70
Maren <mary_dietrich@gmx.de>
2014-06-03 21:18:22 +0200

    CHanged: index.js, Ausdrücke in var geändert, Aufgabe2 - mdietrich

--- Meilenstein 2-3/js/index.js
+++ Meilenstein 2-3/js/index.js
@@ -1,32 +1,36 @@
-function pruefen() {
+function checkForm() {
     var gueltig = true;
+    var handyRegEx = "0+([0-9])";
+    var matrikelRegEx = "([0-9]){7}";
+    var mailRegEx = "^[a-zA-Z0-9äöü._%+-]+@[a-zA-Z0-9äöüÄÖÜ.-]+.[com|de|org|net]$";
+    var nachnameRegEx = "([a-zA-ZäöüÄÖÜ])+";
+    var vornameRegEx = "([a-zA-ZäöüÄÖÜ])+";
 
-    if (!$("#handynummer").val().match("0+([0-9])")) {
+    if (!$("#handynummer").val().match(handyRegEx)) {
         borderColor(handynummer);
         handynummer.focus();
         gueltig = false;
     }
 
-    if (!$("#matrikelnummer").val().match("([0-9]){7}")) {
+    if (!$("#matrikelnummer").val().match(matrikelRegEx)) {
         borderColor(matrikelnummer);
         matrikelnummer.focus();
         gueltig = false;
     }
 
-    var filter = '^[a-zA-Z0-9äöü._%+-]+@[a-zA-Z0-9äöüÄÖÜ.-]+.[com|de|org|net]$';
-    if (!$("#mail").val().match(filter)) {
+    if (!$("#mail").val().match(mailRegEx)) {
         borderColor(mail);
         mail.focus();
         gueltig = false;
     }
 
-    if (!$("#nachname").val().match("([a-zA-ZäöüÄÖÜ])+")) {
+    if (!$("#nachname").val().match(nachnameRegEx)) {
         borderColor(nachname);
         nachname.focus();
         gueltig = false;
     }
 
-    if (!$("#vorname").val().match("([a-zA-ZäöüÄÖÜ])+")) {
+    if (!$("#vorname").val().match(vornameRegEx)) {
         borderColor(vorname);
         vorname.focus();
         gueltig = false;
