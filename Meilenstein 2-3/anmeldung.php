b37daa15ea2ae4c39e95b9eac20405e615092167
SebastianAurin <sebastianaurin@hotmail.com>
2014-06-19 14:23:07 +0200

    CHANGED: anmeldung.php, Schreibungsschema (für *.txt) angepasst, - s.aurin

--- Meilenstein 2-3/anmeldung.php
+++ Meilenstein 2-3/anmeldung.php
@@ -3,8 +3,7 @@
 $my_file = 'C:\\xampp\\htdocs\\team42\\team42\\Meilenstein 2-3\\anmeldeDaten\\' . $_GET["messe"] . '.txt';
 $handle = fopen($my_file,"a") or die('Cannot open file:  ' . $_GET["messe"] . '.txt');
 
-$data =  "Willkommen " . $_GET["name"] . "\nDeine E-Mail Adresse lautet: " . $_GET["email"] . "\nDeine MatrikelNr ist: " . $_GET["matrikelnr"] . "\n" .
-                "Du bist Studiengang: " . $_GET["studiengang"] . "\nDu meldest dich für die: " . $_GET["messe"] . " an" . "\nDeine HandyNr ist: " . $_GET["handy"]."\n";
+$data =  $_GET["vorname"]." ".$_GET["name"].", ".  $_GET["matrikelnr"] .", ". $_GET["email"].", " . $_GET["handy"].", " . $_GET["studiengang"].";\n";
 
 fwrite($handle, $data);
 
