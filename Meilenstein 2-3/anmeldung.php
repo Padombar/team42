e7f5277b9882afe44c2e9ebce2fdcadedf6cdd0a
SebastianAurin <sebastianaurin@hotmail.com>
2014-06-19 14:07:21 +0200

    Changed: anmeldung.php u. index.css u. index.html u. messe.css, Aufgabe 1 fertig u. pfade angepasst, -s.aurin
    Aufpassen, kann sein das die pfadangaben für den projekt ordner bei euch
    nicht stimmen.  ist 2 mal
    
    (/../team42/team42/...)
    
    in der messe.css, index.css, index.html, anmeldung.php

--- Meilenstein 2-3/anmeldung.php
+++ Meilenstein 2-3/anmeldung.php
@@ -1,13 +1,10 @@
 <?php
 
-$handle = fopen('http://localhost/team42/Meilenstein%202-3/anmeldeDaten/' . $_GET["messe"] . '.txt', 'w') or die('Cannot open file:  ' . $_GET["messe"] . '.txt');
+$my_file = 'C:\\xampp\\htdocs\\team42\\team42\\Meilenstein 2-3\\anmeldeDaten\\' . $_GET["messe"] . '.txt';
+$handle = fopen($my_file,"a") or die('Cannot open file:  ' . $_GET["messe"] . '.txt');
 
-$data =  "Willkommen" . $_GET["name"] . "\n" .
-                "Deine E-Mail Adresse lautet:" . $_GET["email"] . "\n" .
-                "Deine MatrikelNr ist:" . $_GET["matrikelnr"] . "\n" .
-                "Du bist Studiengang:" . $_GET["studiengang"] . "\n" .
-                "Du meldest dich für die:" . $_GET["messe"] . an . "\n" .
-                "Deine HandyNr ist:" . $_GET["handy"];
+$data =  "Willkommen " . $_GET["name"] . "\nDeine E-Mail Adresse lautet: " . $_GET["email"] . "\nDeine MatrikelNr ist: " . $_GET["matrikelnr"] . "\n" .
+                "Du bist Studiengang: " . $_GET["studiengang"] . "\nDu meldest dich für die: " . $_GET["messe"] . " an" . "\nDeine HandyNr ist: " . $_GET["handy"]."\n";
 
 fwrite($handle, $data);
 
