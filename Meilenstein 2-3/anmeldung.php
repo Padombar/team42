3c1578d2e9180cf26bd4bde526140d32f1773c70
Maren <mary_dietrich@gmx.de>
2014-06-19 23:41:37 +0200

    Changed: getDetail.php, detail.js, php hinzugefügt, js um Methode erweitert, Aufgabe 5.2 - mdietrich

--- Meilenstein 2-3/anmeldung.php
+++ Meilenstein 2-3/anmeldung.php
@@ -3,7 +3,8 @@
 $my_file = 'C:\\xampp\\htdocs\\team42\\team42\\Meilenstein 2-3\\anmeldeDaten\\' . $_GET["messe"] . '.txt';
 $handle = fopen($my_file,"a") or die('Cannot open file:  ' . $_GET["messe"] . '.txt');
 
-$data =  $_GET["vorname"]." ".$_GET["name"].", ".  $_GET["matrikelnr"] .", ". $_GET["email"].", " . $_GET["handy"].", " . $_GET["studiengang"].";\n";
+$data =  $_GET["vorname"]." ".$_GET["name"].", ".  $_GET["matrikelnr"] .", ". $_GET["email"].",
+ " . $_GET["handy"].", " . $_GET["studiengang"].";\n";
 
 fwrite($handle, $data);
 
---
+++ Meilenstein 2-3/getDetail.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maren, Philip
+ * Date: 19.06.14
+ * Time: 16:19
+ */
+
+header("Content-type: application/json");
+
+file_get_contents('C:\\xampp\\htdocs\\team42\\team42\\Meilenstein 2-3\\anmeldeDaten\\cebit.json');
+file_get_contents('C:\\xampp\\htdocs\\team42\\team42\\Meilenstein 2-3\\anmeldeDaten\\conhit.json');
+file_get_contents('C:\\xampp\\htdocs\\team42\\team42\\Meilenstein 2-3\\anmeldeDaten\\kosten.json');
+file_get_contents('C:\\xampp\\htdocs\\team42\\team42\\Meilenstein 2-3\\anmeldeDaten\\webtech.json');
+
+
+
+
+?>
